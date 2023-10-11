11538170516c46f908fb81f4e6126b7a23118a7d
Kokorev-VD <digitalfalk@mail.ru>
2023-10-11 17:40:03 +0300

    Added Tickets to DB

--- src/main/kotlin/com/mephistophels/rjd/database/entity/Companion.kt
+++ src/main/kotlin/com/mephistophels/rjd/database/entity/Companion.kt
@@ -39,4 +39,6 @@ class Companion(
     @OneToMany(mappedBy = "companion", cascade = [CascadeType.ALL])
     var mark: Set<Mark> = HashSet()
 
+    @OneToMany(mappedBy = "companion", cascade = [CascadeType.ALL])
+    var tickets: Set<Ticket> = HashSet()
 }
